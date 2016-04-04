d6350e265847becfc73b8ab444dbe98fd7a1e1db
Elad Douenias <eladd@algotec.co.il>
2016-04-04 14:29:06 +0300

    fixes and refactor to typescript

--- src/kendo.component.ts
+++ src/kendo.component.ts
@@ -17,6 +17,7 @@ export class KendoComponent {
             console.error('widgetName is required for KendoComponent');
         }
     }
+    
     render() {
         this._kWidget = jQuery(this._element)[this.widgetName](this.bound);
         this._kendoKeys = [];
@@ -24,6 +25,7 @@ export class KendoComponent {
             this._kendoKeys.push(key);
         }
     }
+    
     ngOnChanges(changes) {
         if(changes.value) {
             !this._kWidget && this.render()
@@ -52,31 +54,17 @@ export class KendoComponent {
         }
         this.render();
     }
+    
     ngOnInit() {
         !this._hadFirstRender && this.render();
     }
+    
     ngOnDestroy() {
         var kElement = jQuery(this._element);
         kElement.data(this.widgetName).destroy();
         kElement.empty();
     }
 };
-
-// function createCommonKendoComponent(selector: string, widgetName: string) {
-//     @Component({
-//         selector: selector,
-//         template: '<ng-content></ng-content>'
-//     })
-//     class CommonComponent extends KendoComponent {
-//         @Input() bound;
-//         @Input() role: string;
-        
-//         constructor(elementRef: ElementRef) {            
-//             super(elementRef, widgetName);
-//         }
-//     }
-//     return CommonComponent;
-// }
 
 const template = '<ng-content></ng-content>';
 
@@ -174,52 +162,3 @@ export class KendoDiagram extends KendoComponent { @Input() bound; @Input() role
 export class KendoMap extends KendoComponent { @Input() bound; @Input() role: string; @Input() value; constructor(elementRef: ElementRef) { super(elementRef, 'kendoMap'); } }
 @Component({ selector: '[data-role=flatcolorpicker]', template })
 export class KendoFlatColorPicker extends KendoComponent { @Input() bound; @Input() role: string; @Input() value; constructor(elementRef: ElementRef) { super(elementRef, 'kendoFlatColorPicker'); } }
-
-
-// export var KendoDropDownList = createCommonKendoComponent('[data-role=dropdownlist]', 'kendoDropDownList');
-// export var KendoDatePicker = createCommonKendoComponent('[data-role=datepicker]', 'kendoDatePicker');
-// export var KendoAutoComplete = createCommonKendoComponent('[data-role=autocomplete]', 'kendoAutoComplete');
-// export var KendoButton = createCommonKendoComponent('[data-role=button]', 'kendoButton');
-// export var KendoColorPalette = createCommonKendoComponent('[data-role=colorpalette]', 'kendoColorPalette');
-// export var KendoColorPicker = createCommonKendoComponent('[data-role=colorpicker]', 'kendoColorPicker');
-// export var KendoDateTimePicker = createCommonKendoComponent('[data-role=datetimepicker]', 'kendoDateTimePicker');
-// export var KendoEditor = createCommonKendoComponent('[data-role=editor]', 'kendoEditor');
-// export var KendoMaskedTextBox = createCommonKendoComponent('[data-role=maskedtextbox]', 'kendoMaskedTextBox');
-// export var KendoMultiSelect = createCommonKendoComponent('[data-role=multiselect]', 'kendoMultiSelect');
-// export var KendoNumericTextBox = createCommonKendoComponent('[data-role=numerictextbox]', 'kendoNumericTextBox');
-// export var KendoSlider = createCommonKendoComponent('[data-role=slider]', 'kendoSlider');
-// export var KendoTimePicker = createCommonKendoComponent('[data-role=timepicker]', 'kendoTimePicker');
-// export var KendoUpload = createCommonKendoComponent('[data-role=upload]', 'kendoUpload');
-// export var KendoMobileSwitch = createCommonKendoComponent('[data-role=mobileswitch]', 'kendoMobileSwitch');
-// export var KendoMobileButtonGroup = createCommonKendoComponent('[data-role=mobilebuttongroup]', 'kendoMobileButtonGroup');
-// export var KendoMenu = createCommonKendoComponent('[data-role=menu]', 'kendoMenu');
-// export var KendoPanelBar = createCommonKendoComponent('[data-role=panelbar]', 'kendoPanelBar');
-// export var KendoTabStrip = createCommonKendoComponent('[data-role=tabstrip]', 'kendoTabStrip');
-// export var KendoToolBar = createCommonKendoComponent('[data-role=toolbar]', 'kendoToolBar');
-// export var KendoTreeView = createCommonKendoComponent('[data-role=treeview]', 'kendoTreeView');
-// export var KendoCalendar = createCommonKendoComponent('[data-role=calendar]', 'kendoCalendar');
-// export var KendoGantt = createCommonKendoComponent('[data-role=gantt]', 'kendoGantt');
-// export var KendoScheduler = createCommonKendoComponent('[data-role=scheduler]', 'kendoScheduler');
-// export var KendoGrid = createCommonKendoComponent('[data-role=grid]', 'kendoGrid');
-// export var KendoListView = createCommonKendoComponent('[data-role=listview]', 'kendoListView');
-// export var KendoPager = createCommonKendoComponent('[data-role=pager]', 'kendoPager');
-// export var KendoPivotGrid = createCommonKendoComponent('[data-role=pivotgrid]', 'kendoPivotGrid');
-// export var KendoPivotConfigurator = createCommonKendoComponent('[data-role=pivotconfigurator]', 'kendoPivotConfigurator');
-// export var KendoTreeList = createCommonKendoComponent('[data-role=treelist]', 'kendoTreeList');
-// export var KendoNotification = createCommonKendoComponent('[data-role=notification]', 'kendoNotification');
-// export var KendoResponsivePanel = createCommonKendoComponent('[data-role=responsivepanel]', 'kendoResponsivePanel');
-// export var KendoSplitter = createCommonKendoComponent('[data-role=splitter]', 'kendoSplitter');
-// export var KendoToolTip = createCommonKendoComponent('[data-role=tooltip]', 'kendoToolTip');
-// export var KendoWindow = createCommonKendoComponent('[data-role=window]', 'kendoWindow');
-// export var KendoDraggable = createCommonKendoComponent('[data-role=draggable]', 'kendoDraggable');
-// export var KendoDropTarget = createCommonKendoComponent('[data-role=droptarget]', 'kendoDropTarget');
-// export var KendoProgressBar = createCommonKendoComponent('[data-role=progressbar]', 'kendoProgressBar');
-// export var KendoSortable = createCommonKendoComponent('[data-role=sortable]', 'kendoSortable');
-// export var KendoChart = createCommonKendoComponent('[data-role=chart]', 'kendoChart');
-// export var KendoBarCode = createCommonKendoComponent('[data-role=barcode]', 'kendoBarCode');
-// export var KendoQRCode = createCommonKendoComponent('[data-role=qrcode]', 'kendoQRCode');
-// export var KendoLinearGuage = createCommonKendoComponent('[data-role=linearguage]', 'kendoLinearGuage');
-// export var KendoRadialGuage = createCommonKendoComponent('[data-role=radialguage]', 'kendoRadialGuage');
-// export var KendoDiagram = createCommonKendoComponent('[data-role=diagram]', 'kendoDiagram');
-// export var KendoMap = createCommonKendoComponent('[data-role=map]', 'kendoMap');
-// export var KendoFlatColorPicker = createCommonKendoComponent('[data-role=flatcolorpicker]', 'kendoFlatColorPicker');
